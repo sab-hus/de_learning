52dd0c88d17c4fdda408df23cac6fc912ccb1bc5
sabah-hussain <sabah.hussain@datatonic.com>
2023-09-07 14:46:51 +0100

    feature(wip): updated schema validation process to integrate into load process

--- batch_processing/csv_loads/schema_validation.py
+++ batch_processing/csv_loads/schema_validation.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import pandera as pa
 import logging
+from file_names import input_file, output_file
 
 logging.basicConfig(level=logging.INFO)
 
@@ -30,25 +31,23 @@ def convert_data_types(input_file, output_file):
     except Exception as e:
         logging.error(f"An error occurred: {str(e)}")
 
-input_file = 'mock_orders.csv'
-output_file = 'mock_orders_updated.csv'
 convert_data_types(input_file, output_file)
 
-# def validate_mock_orders_schema(csv_file_path):
-#     try:
-#         df = pd.read_csv(csv_file_path)
-#         schema = pa.DataFrameSchema({
-#             "contribution": pa.Column(pa.Float, required=True, nullable=False),
-#             "billed_date": pa.Column(pa.DateTime, required=True, nullable=False),
-#             "order_id": pa.Column(pa.String, required=True, nullable=False)
-#         })
-#         schema.validate(df, lazy=True)
-#         logging.info("Validation successful for all columns.")    
-#         # return validated df
+def validate_mock_orders_schema(csv_file_path):
+    try:
+        df = pd.read_csv(csv_file_path)
+        schema = pa.DataFrameSchema({
+            "contribution": pa.Column(pa.Float, required=True, nullable=False),
+            "billed_date": pa.Column(pa.DateTime, required=True, nullable=False),
+            "order_id": pa.Column(pa.String, required=True, nullable=False)
+        })
+        schema.validate(df, lazy=True)
+        logging.info("Validation successful for all columns.")    
+        # return validated df
     
-#     except pa.errors.SchemaErrors as err:
-#         logging.error(f"Dataframe validation failed. Errors:\n{err.failure_cases}")
-#         logging.error(f"Invalid data rows:\n{err.data}")
+    except pa.errors.SchemaErrors as err:
+        logging.error(f"Dataframe validation failed. Errors:\n{err.failure_cases}")
+        logging.error(f"Invalid data rows:\n{err.data}")
 
-# csv_file_path = "mock_orders_updated.csv"
-# validated_df = validate_mock_orders_schema(csv_file_path)
+csv_file_path = output_file
+validate_mock_orders_schema(csv_file_path)
